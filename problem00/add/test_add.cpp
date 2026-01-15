f67fa0c01043e3b75a1044205e3aac748c6899d9
Adam <radam05@icloud.com>
2026-01-15 13:54:45 -0500

    added double test case

--- problem00/add/test_add.cpp
+++ problem00/add/test_add.cpp
@@ -5,10 +5,17 @@
 
 /* Provided tests */
 
-TEST_CASE("Test add", "[Add]"){
+TEST_CASE("Test add", "[Add]")
+{
 
   Add<int> a;
   REQUIRE(a.call(1,2)==3);
 }
 
 /* Your tests here */
+
+TEST_CASE("Test add double", "[Add]")
+{
+  Add<double> b;
+  REQUIRE(b.call(1.5,2.5)==4.0);
+}
