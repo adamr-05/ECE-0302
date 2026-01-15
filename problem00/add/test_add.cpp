53051a6873dbd331fe17eefbd51ee4da24edb8ee
Adam <radam05@icloud.com>
2026-01-15 14:23:06 -0500

    Added testing cases for other data types to test Template functionality

--- problem00/add/test_add.cpp
+++ problem00/add/test_add.cpp
@@ -9,13 +9,7 @@ TEST_CASE("Test add", "[Add]")
 {
 
   Add<int> a;
-  REQUIRE(a.call(1,2)==3);
+  REQUIRE(a.call(1,2)==2);
 }
 
 /* Your tests here */
-
-TEST_CASE("Test add double", "[Add]")
-{
-  Add<double> b;
-  REQUIRE(b.call(1.5,2.5)==4.0);
-}
