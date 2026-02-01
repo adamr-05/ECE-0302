bbcc683dfb52cde681e158b821ce95219610c2f5
Adam <radam05@icloud.com>
2026-02-01 18:49:20 -0500

    add testing cases for LimitedSizeBag

--- problem01/bags/test_LimitedSizeBag.cpp
+++ problem01/bags/test_LimitedSizeBag.cpp
@@ -50,3 +50,89 @@ TEST_CASE("add multiple bag items", "[LimitedSizeBag]")
   REQUIRE(bag.contains(302) == true);
   REQUIRE(bag.contains(402) == true);
 }
+
+TEST_CASE("add duplicates", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> bag;
+  
+  REQUIRE(bag.add(302) == true);
+  REQUIRE(bag.add(302) == true);
+  REQUIRE(bag.add(302) == true);
+
+  REQUIRE(bag.getCurrentSize() == 3);
+  REQUIRE(bag.contains(302) == true);
+  REQUIRE(bag.getFrequencyOf(302) == 3);
+}
+
+TEST_CASE("add to max size", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> bag;
+
+  for (std::size_t i = 0; i < 100; i++)
+  {
+    REQUIRE(bag.add(i) == true);
+  }
+  REQUIRE(bag.getCurrentSize() == 100);
+
+  REQUIRE_FALSE(bag.add(100) == true); // add fails when bag is full
+  REQUIRE_FALSE(bag.contains(100) == true);
+  REQUIRE_FALSE(bag.getCurrentSize() == 101);
+}
+
+TEST_CASE("remove item", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> bag;
+
+  REQUIRE(bag.add(102) == true);
+  REQUIRE(bag.add(202) == true);
+  REQUIRE(bag.add(302) == true);
+
+  REQUIRE(bag.remove(302) == true);
+  REQUIRE(bag.getCurrentSize() == 2);
+  REQUIRE(bag.contains(302) == false);
+
+  REQUIRE_FALSE(bag.remove(302) == true); //cant remove item that isnt there
+}
+
+TEST_CASE("clear bag", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> bag;
+
+  REQUIRE(bag.add(102) == true);
+  REQUIRE(bag.add(202) == true);
+  REQUIRE(bag.add(302) == true);
+
+  bag.clear();
+
+  REQUIRE(bag.isEmpty() == true);
+  REQUIRE(bag.getCurrentSize() == 0);
+  REQUIRE(bag.contains(302) == false);
+
+  REQUIRE(bag.add(302) == true);
+  REQUIRE(bag.isEmpty() == false);
+  REQUIRE(bag.contains(302) == true);
+  REQUIRE(bag.getCurrentSize() == 1);
+}
+
+TEST_CASE("test other types", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<std::string> bag;
+
+  REQUIRE(bag.add("hello") == true);
+  REQUIRE(bag.add("how are you") == true);
+  REQUIRE(bag.add("goodbye") == true);
+
+  REQUIRE(bag.isEmpty() == false);
+  REQUIRE(bag.getCurrentSize() == 3);
+  REQUIRE(bag.contains("hello") == true);
+  REQUIRE(bag.contains("how are you") == true);
+
+  REQUIRE(bag.getFrequencyOf("hello") == 1);
+  REQUIRE(bag.remove("hello") ==true);
+  REQUIRE(bag.contains("hello") == false);
+
+  bag.clear();
+
+  REQUIRE(bag.isEmpty() == true);
+}
+  
