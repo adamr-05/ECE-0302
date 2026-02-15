7ce9606338e20a4ca2cd8505c0356398a2966315
Adam <radam05@icloud.com>
2026-02-15 17:00:44 -0500

    add recursive postfixtoprefix and setfrompostfix functions

--- problem03/postpre/Expression.cpp
+++ problem03/postpre/Expression.cpp
@@ -27,8 +27,16 @@ void Expression::setFromPrefix(const std::string &pre)
 void Expression::setFromPostfix(const std::string &post)
 {
   // TODO: check format
+  if (isPost(post) == false)
+  {
+    throw std::invalid_argument("invalid postfix expression");
+  }
 
   // TODO: store prefix and postfix forms
+
+  postfixExpression = post;
+  prefixExpression.clear();
+  postfixToPrefix(postfixExpression, prefixExpression);
 
 }
 
@@ -66,6 +74,20 @@ void Expression::prefixToPostfix(const std::string &prefix, std::string &postfix
 void Expression::postfixToPrefix(const std::string &postfix, std::string &prefix) const {
 
   // TODO: recursive function
+  int end1, end2;
+  char last_char = postfix[postfix.length() - 1];
+
+  if (isOperator(last_char))
+  {
+    prefix += last_char;
+
+    end1 = endPost(postfix, postfix.length() - 2);
+    end2 = endPost(postfix, end1-1);
+
+    postfixToPrefix(postfix.substr(end2, end1 - end2), prefix);
+    postfixToPrefix(postfix.substr(end1, postfix.length() - 1 - end1), prefix);
+  }
+  else prefix += last_char;
 
 }
 
