0e4cf71a12ea74843b5e1baa050e7bf94f0cdc35
Adam <radam05@icloud.com>
2026-02-15 16:37:55 -0500

    add 2 private members to expression

--- problem03/postpre/Expression.hpp
+++ problem03/postpre/Expression.hpp
@@ -39,6 +39,8 @@ class Expression : public AbstractExpression {
 
 
         // TODO: members for stored expression
+        std::string prefixExpression;
+        std::string postfixExpression;
 
 };
 
