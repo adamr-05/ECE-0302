1d88477c1c60f76d0e8f11272e1755a4bd7e57d7
Adam <radam05@icloud.com>
2026-02-23 17:13:55 -0500

    add comments

--- project2/project2/test_FindPalindrome.cpp
+++ project2/project2/test_FindPalindrome.cpp
@@ -3,11 +3,41 @@
 #include "catch.hpp"
 #include "FindPalindrome.hpp"
 
-/*first i implemented the add function because that seemed the simplest and the valildations seemed easy for me to check using isalpha and checking uniqueness
-with loops. i added the private methods here, i realized that i definitely need a vector to store all the data from the words and then thought of a way to
-hold all the successful palindrome finds and determined i could create a vector of the vectors, to store all the ones that are palindromes. then i added some
-testing cases to verify functionality of add. From here i realized implementing Number would be really simple with my design, only returning size of the
-palindrome vector of vectors, where every vector of  vectors in it would be the palindrome.
+/*
+
+first i implemented the add function because that seemed the simplest and the valildations seemed easy for me to check using isalpha and checking uniqueness
+with loops.
+
+i then added the private methods, i realized that i definitely need a vector to store all the data from the words and then thought of a way to
+hold all the successful palindrome finds and determined i could create a vector of the vectors, to store all the ones that are palindromes. 
+
+then i added some testing cases to verify functionality of add. From here i realized implementing Number would be really simple with my design, only returning 
+size of the palindrome vector of vectors, where every vector of vectors in it would be the palindrome. 
+
+from here i moved on the the recursive palindrome, which was easily the most difficult part. i started with base case where remaining was N. checking if 
+remaining words is empty, and if so combine candidate into a string and checking palindromity of it. if it was it got added the the palindromes vector,
+which holds all the palindromes. then i wrote a recursive case that tries each word in remainingWords and adds to the candidate, then creates a new 
+remainingWords vector without the word and recursively calls. using pop_back after each call allowed me to undo that one so i could test another word.
+
+at first my test cases returned 0 palindromes after this, but i realized i wasn't calling the  recursive function from the add function. i added that and
+made sure to clear old palindromes before updating it.
+
+Then i implemented the other add method, which was similar to the first with error checking, such as checking alpha chars, but the uniqueness had to be
+compared against itself and against the existing vector.
+
+toVector just returns my private member palindromes.
+
+Clear also just cleared both datas.
+
+Finally I did the cutTests. 
+
+cutTest1 i decided to
+first combine all words into one long string to work with. then i could create an array with one cell for every alphabet letter. Then i could check the
+frequency of every letter. i checked there was at most one letter with odd count. 
+
+For cutTest2 it was kind of similar but comparing the two strings this time.
+converting the data was all almost the same, but instead of even/odd i compared character counts of the smaller to the larger, where i made sure every letter
+in the smaller string appears at least as many times in the larger.
 
 
 
