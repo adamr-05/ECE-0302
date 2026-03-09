cc6884bb2d697b1d36241cfafac4ff1b491fcde5
Adam <radam05@icloud.com>
2026-03-08 23:07:27 -0400

    add private mems in list.hpp

--- problem05/sorted_list/List.hpp
+++ problem05/sorted_list/List.hpp
@@ -34,7 +34,9 @@ public:
 
 private:
 
-  //TODO
+  T* items;                 //holds array data
+  std::size_t length;       //length of current array
+  std::size_t capacity;     //memory allocated for aarray (increase memory allocated if length gets close to or exceeds capacity size)
   
 };
 
