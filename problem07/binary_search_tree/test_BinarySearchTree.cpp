a9fff7c5e85fd78e7addbfa262d7e73ce68f94de
Adam <radam05@icloud.com>
2026-04-06 17:21:38 -0400

    add testing cases

--- problem07/binary_search_tree/test_BinarySearchTree.cpp
+++ problem07/binary_search_tree/test_BinarySearchTree.cpp
@@ -75,3 +75,172 @@ TEST_CASE("BST: Test Copy Assign", "[copy assign]") {
 }
 
 /* Your test cases here */
+
+TEST_CASE("BST: Insert into empty tree", "[insert]") {
+    BinarySearchTree<int, int> bst;
+    REQUIRE(bst.isEmpty());
+    REQUIRE(bst.insert(10, 10));
+    REQUIRE_FALSE(bst.isEmpty());
+    int item;
+    REQUIRE(bst.retrieve(10, item));
+    REQUIRE(item == 10);
+}
+
+TEST_CASE("BST: Insert duplicate", "[insert]") {
+    BinarySearchTree<int, int> bst;
+    REQUIRE(bst.insert(5, 5));
+    REQUIRE_FALSE(bst.insert(5, 5));
+}
+
+TEST_CASE("BST: Insert ascending order", "[insert]") {
+    BinarySearchTree<int, int> bst;
+    for (int i = 1; i <= 5; i++)
+        REQUIRE(bst.insert(i, i));
+    int item;
+    for (int i = 1; i <= 5; i++)
+        REQUIRE(bst.retrieve(i, item));
+}
+
+TEST_CASE("BST: Insert descending order", "[insert]") {
+    BinarySearchTree<int, int> bst;
+    for (int i = 5; i >= 1; i--)
+        REQUIRE(bst.insert(i, i));
+    int item;
+    for (int i = 1; i <= 5; i++)
+        REQUIRE(bst.retrieve(i, item));
+}
+
+TEST_CASE("BST: Remove from empty tree", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    REQUIRE_FALSE(bst.remove(5));
+}
+
+TEST_CASE("BST: Remove nonexistent key", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    REQUIRE_FALSE(bst.remove(99));
+}
+
+TEST_CASE("BST: Remove single node (root leaf)", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    REQUIRE(bst.remove(10));
+    REQUIRE(bst.isEmpty());
+}
+
+TEST_CASE("BST: Remove leaf node", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(15, 15);
+    REQUIRE(bst.remove(5));
+    int item;
+    REQUIRE_FALSE(bst.retrieve(5, item));
+    REQUIRE(bst.retrieve(10, item));
+    REQUIRE(bst.retrieve(15, item));
+}
+
+TEST_CASE("BST: Remove node with only right child", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(7, 7);
+    REQUIRE(bst.remove(5));
+    int item;
+    REQUIRE_FALSE(bst.retrieve(5, item));
+    REQUIRE(bst.retrieve(7, item));
+    REQUIRE(bst.retrieve(10, item));
+}
+
+TEST_CASE("BST: Remove node with only left child", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(3, 3);
+    REQUIRE(bst.remove(5));
+    int item;
+    REQUIRE_FALSE(bst.retrieve(5, item));
+    REQUIRE(bst.retrieve(3, item));
+    REQUIRE(bst.retrieve(10, item));
+}
+
+TEST_CASE("BST: Remove node with two children", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(15, 15);
+    bst.insert(3, 3);
+    bst.insert(7, 7);
+    REQUIRE(bst.remove(5));
+    int item;
+    REQUIRE_FALSE(bst.retrieve(5, item));
+    REQUIRE(bst.retrieve(3, item));
+    REQUIRE(bst.retrieve(7, item));
+    REQUIRE(bst.retrieve(10, item));
+    REQUIRE(bst.retrieve(15, item));
+}
+
+TEST_CASE("BST: Remove root with two children", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(15, 15);
+    REQUIRE(bst.remove(10));
+    int item;
+    REQUIRE_FALSE(bst.retrieve(10, item));
+    REQUIRE(bst.retrieve(5, item));
+    REQUIRE(bst.retrieve(15, item));
+}
+
+TEST_CASE("BST: Remove all nodes", "[remove]") {
+    BinarySearchTree<int, int> bst;
+    bst.insert(10, 10);
+    bst.insert(5, 5);
+    bst.insert(15, 15);
+    bst.insert(3, 3);
+    bst.insert(7, 7);
+    REQUIRE(bst.remove(3));
+    REQUIRE(bst.remove(7));
+    REQUIRE(bst.remove(5));
+    REQUIRE(bst.remove(15));
+    REQUIRE(bst.remove(10));
+    REQUIRE(bst.isEmpty());
+}
+
+TEST_CASE("BST: Copy constructor", "[copy]") {
+    BinarySearchTree<int, int> bst1;
+    bst1.insert(10, 10);
+    bst1.insert(5, 5);
+    bst1.insert(15, 15);
+
+    BinarySearchTree<int, int> bst2(bst1);
+    bst1.destroy();
+    REQUIRE(bst1.isEmpty());
+    REQUIRE_FALSE(bst2.isEmpty());
+
+    int item;
+    REQUIRE(bst2.retrieve(10, item));
+    REQUIRE(bst2.retrieve(5, item));
+    REQUIRE(bst2.retrieve(15, item));
+}
+
+TEST_CASE("BST: TreeSort", "[treesort]") {
+    BinarySearchTree<int, int> bst;
+    int arr[] = {8, 3, 1, 6, 4, 7, 10, 14, 13};
+    bst.treeSort(arr, 9);
+    REQUIRE(arr[0] == 1);
+    REQUIRE(arr[1] == 3);
+    REQUIRE(arr[2] == 4);
+    REQUIRE(arr[3] == 6);
+    REQUIRE(arr[4] == 7);
+    REQUIRE(arr[5] == 8);
+    REQUIRE(arr[6] == 10);
+    REQUIRE(arr[7] == 13);
+    REQUIRE(arr[8] == 14);
+}
+
+TEST_CASE("BST: TreeSort duplicates throw", "[treesort]") {
+    BinarySearchTree<int, int> bst;
+    int arr[] = {5, 3, 5};
+    REQUIRE_THROWS(bst.treeSort(arr, 3));
+}
