fee764701392c15b9e99e39211414c66586fdf61
Adam <radam05@icloud.com>
2026-04-14 17:46:45 -0400

    implemented BFS and add comments

--- project4/project4/pathfinder.cpp
+++ project4/project4/pathfinder.cpp
@@ -78,65 +78,141 @@ int main(int argc, char *argv[])
   //enqueue start State to frontier
   frontier.enqueue(State(start_row, start_col));
 
-   if (start_row == 0 || start_row == image.height() - 1 ||
+  //edge case -- start pixel is already at exit
+  if (start_row == 0 || start_row == image.height() - 1 ||
       start_col == 0 || start_col == image.width() - 1)
   {
-    image(start_row, start_col) = GREEN;
-    writeToFile(image, output_file);
+    image(start_row, start_col) = GREEN; //set pixel to green
+    writeToFile(image, output_file);    //write final image
     std::cout << "Solution Found" << std::endl;
     return EXIT_SUCCESS;
   }
 
   while (frontier.isEmpty() == false)
   {
+    //grab next State coordinates from frontier queue, then remove it
     State currentState = frontier.peekFront();
     frontier.dequeue();
 
-    //UP
+    //check its neighbors (up down left right)
+    
+    //UP conditions. ROW > 0, at top cant go up, not visited yet, and white (empty space -- not a wall)
     if (currentState.first > 0 &&
         !visited[currentState.first - 1][currentState.second] &&
         image(currentState.first - 1, currentState.second) == WHITE)
     {
+      //if at edge (solution found)
+      //get nextrow and nextcolumn values
       std::size_t nr = currentState.first - 1;
       std::size_t nc = currentState.second;
 
       if (nr == 0 || nr == image.height() - 1 ||
           nc == 0 || nc == image.width() - 1)
       {
-        image(nr, nc) = GREEN;
-        writeToFile(image, output_file);
+        image(nr, nc) = GREEN; //set pixel to green
+        writeToFile(image, output_file);    //write final image
         std::cout << "Solution Found" << std::endl;
         return EXIT_SUCCESS;
       }
 
-      visited[nr][nc] = true;
-      frontier.enqueue(State(nr, nc));
+      // otherwise mark visited and enqueue
+      {
+        visited[nr][nc] = true;
+        frontier.enqueue(State(nr, nc));
+      }
     }
 
-    //DOWN
+
+
+
+
+
+
+    //DOWN conditions. ROW < edge, at bottom cant go down, not visited yet, and white (empty space -- not a wall)
     if (currentState.first < image.height() - 1 &&
         !visited[currentState.first + 1][currentState.second] &&
         image(currentState.first + 1, currentState.second) == WHITE)
     {
+      //if at edge (solution found)
+      //get nextrow and nextcolumn values
       std::size_t nr = currentState.first + 1;
       std::size_t nc = currentState.second;
 
       if (nr == 0 || nr == image.height() - 1 ||
           nc == 0 || nc == image.width() - 1)
       {
-        image(nr, nc) = GREEN;
-        writeToFile(image, output_file);
+        image(nr, nc) = GREEN; //set pixel to green
+        writeToFile(image, output_file);    //write final image
         std::cout << "Solution Found" << std::endl;
         return EXIT_SUCCESS;
       }
 
-      visited[nr][nc] = true;
-      frontier.enqueue(State(nr, nc));
+      // otherwise mark visited and enqueue
+      {
+        visited[nr][nc] = true;
+        frontier.enqueue(State(nr, nc));
+      }
     }
 
-    // TODO: LEFT and RIGHT neighbors
+
+
+    //LEFT conditions. COL > 0, at left edge cant go left, not visited yet, and white (empty space -- not a wall)
+    if (currentState.second > 0 &&
+        !visited[currentState.first][currentState.second - 1] &&
+        image(currentState.first, currentState.second - 1) == WHITE)
+    {
+      //if at edge (solution found)
+      //get nextrow and nextcolumn values
+      std::size_t nr = currentState.first;
+      std::size_t nc = currentState.second - 1;
+
+      if (nr == 0 || nr == image.height() - 1 ||
+          nc == 0 || nc == image.width() - 1)
+      {
+        image(nr, nc) = GREEN; //set pixel to green
+        writeToFile(image, output_file);    //write final image
+        std::cout << "Solution Found" << std::endl;
+        return EXIT_SUCCESS;
+      }
+
+      // otherwise mark visited and enqueue
+      {
+        visited[nr][nc] = true;
+        frontier.enqueue(State(nr, nc));
+      }
+    }
+
+
+
+
+    //RIGHT conditions. COL < edge, at right edge cant go right, not visited yet, and white (empty space -- not a wall)
+    if (currentState.second < image.width() - 1 &&
+        !visited[currentState.first][currentState.second + 1] &&
+        image(currentState.first, currentState.second + 1) == WHITE)
+    {
+      //if at edge (solution found)
+      //get nextrow and nextcolumn values
+      std::size_t nr = currentState.first;
+      std::size_t nc = currentState.second + 1;
+
+      if (nr == 0 || nr == image.height() - 1 ||
+          nc == 0 || nc == image.width() - 1)
+      {
+        image(nr, nc) = GREEN; //set pixel to green
+        writeToFile(image, output_file);    //write final image
+        std::cout << "Solution Found" << std::endl;
+        return EXIT_SUCCESS;
+      }
+
+      // otherwise mark visited and enqueue
+      {
+        visited[nr][nc] = true;
+        frontier.enqueue(State(nr, nc));
+      }
+    }
   }
-
-  // TODO: no solution case
+      //no exit found
+    writeToFile(image, output_file);
+    std::cout << "No Solution Found" << std::endl;
+    return EXIT_SUCCESS;
 }
-
