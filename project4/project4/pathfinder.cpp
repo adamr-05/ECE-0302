09c8b2f8ccde9127b6fb280259d9caea09a165cc
Adam <radam05@icloud.com>
2026-04-14 16:23:17 -0400

    start pixel checking/invalid image checking

--- project4/project4/pathfinder.cpp
+++ project4/project4/pathfinder.cpp
@@ -22,6 +22,50 @@ int main(int argc, char *argv[])
   // Read input image from file
   Image image = readFromFile(input_file);
 
+  //track startrow and startcol, and redpixels
+  std::size_t start_row = 0, start_col = 0;
+  int red_count = 0;
+
+  //loop through entire image
+  for (std::size_t r = 0; r < image.height(); r++)
+  {
+    for (std::size_t c = 0; c < image.width(); c++)
+    {
+      Pixel p = image(r, c);
+      
+      //if pixel is red, startpos found
+      if (p == RED)
+      {
+        start_row = r;
+        start_col = c;
+        red_count++;
+      }
+
+      //if pixel is not red, black, or white, it is invalid
+      else if (p != BLACK && p != WHITE)
+      {
+        std::cerr << "Error: Invaid pixel color in image" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+  }
+
+  //check for multiple red or no red pixels
+  if (red_count == 0)
+  {
+    std::cerr << "Error: No red pixel found (start pixel)" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (red_count > 1)
+  {
+    std::cerr << "Error: Must be only one red start pixel" << std::endl;
+    return EXIT_FAILURE;
+  }
+    
+
+
+
+
   // TODO: breadth-first search
 
   // TODO: Write solution image to file
